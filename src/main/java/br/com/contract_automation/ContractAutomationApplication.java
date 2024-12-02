c3fe39b9be01e3bb747d733982952badf0e18f88
Emm4nuell <eduardo.eesf@gmail.com>
2024-12-01 21:36:29 -0300

    first commit

---
+++ src/main/java/br/com/contract_automation/ContractAutomationApplication.java
@@ -0,0 +1,13 @@
+package br.com.contract_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/contract_automation/ContractAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contract_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
